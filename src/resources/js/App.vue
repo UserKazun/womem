ed6790fa1535d4185ebbd4e3e780ea69799a0b95
UserKazun <developer.kazu0941@gmail.com>
2023-09-17 17:24:00 +0900

    update vue router

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-    import { ref } from 'vue'
+    import Header from './components/Header.vue';
 </script>
 
 <template>
-    <div>
-        <router-view></router-view>
-    </div>
+    <Header />
+    <router-view></router-view>
 </template>
---
+++ src/resources/js/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+    <div class="navbar navbar-expand-md navbar-light bg-white shadow-sm mb-5">
+        <div class="container">
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link">Go to home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/example" class="nav-link">Go to example</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
