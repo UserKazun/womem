d89e94b549d505cfe4fe1e5eba086591fe52e3b4
UserKazun <developer.kazu0941@gmail.com>
2023-11-12 23:01:28 +0900

    update use action and resource

--- src/app/Http/Controllers/MemoController.php
+++ src/app/Http/Controllers/MemoController.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\MemoRequest;
-use App\Services\MemoService;
+use App\Http\Resources\MemoResource;
+use App\Models\Memo;
 use App\Services\FetchAllMemoService;
+use App\UseCases\Memo\RegisterAction;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    public function register(MemoService $service, MemoRequest $request)
+    public function register(RegisterAction $action, MemoRequest $request): MemoResource
     {
-        $result = $service->execute((string)$request->input('title'), (string)$request->input('description'));
-        if ($result) {
-            return response(['message' => 'success'], 200)->header('Content-Type', 'application/json');
-        }
+        return new MemoResource(
+            $action(
+                new Memo(),
+                1,
+                (string)$request->input('title'),
+                (string)$request->input('description')
+            )
+        );
     }
 
     public function fetchAllMemo(FetchAllMemoService $service, Request $request): array
